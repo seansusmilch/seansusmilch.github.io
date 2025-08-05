6f6df93b7203392ad8972ef0a6d1e654beb4b205
seansusmilch <seantsusmilch@gmail.com>
2025-08-04 23:00:30 -0500

    feat: remove giscus comments

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,3 @@
-import type { GiscusProps } from "@giscus/react";
-
 export const SITE = {
   website: "https://seansusmilch.github.io", // replace this with your deployed domain
   author: "Sean",
@@ -23,22 +21,9 @@ export const SITE = {
   lang: "en",
 };
 
-export const GISCUS: GiscusProps = {
-  repo: "seansusmilch/seansusmilch.github.io",
-  repoId: "R_kgDOM6OIrA",
-  category: "Post Comments",
-  categoryId: "DIC_kwDOM6OIrM4ClqDn",
-  mapping: "pathname",
-  reactionsEnabled: "1",
-  emitMetadata: "0",
-  inputPosition: "bottom",
-  lang: "en",
-  loading: "lazy",
-};
-
 export const LOCALE = {
   lang: "en", // html lang code. Set this empty and default will be "en"
-  timezone: "Asia/Bangkok", // Default global timezone (IANA format) https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
+  timezone: "America/Chicago", // Default global timezone (IANA format) https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
 } as const;
 
 export const LOGO_IMAGE = {
